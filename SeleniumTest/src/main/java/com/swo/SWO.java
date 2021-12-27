1425de3a73ec1f5706326c7660635ae0fcb60f7b
Atilla Recep Basaran <atillarbasaran@gmail.com>
2021-12-27 22:44:51 +0300

    start update

--- SeleniumTest/src/main/java/com/swo/SWO.java
+++ SeleniumTest/src/main/java/com/swo/SWO.java
@@ -2,8 +2,6 @@ package com.swo;
 
 import Page.Index;
 import Page.Page;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -12,15 +10,8 @@ import java.io.File;
 
 public class SWO {
     public static void main(String[] args) {
-        LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
-
-        File file = new File("/home/mrbasaran/IdeaProjects/SeleniumTest/src/main/resources/log4j.properties");
-
-        context.setConfigLocation(file.toURI());
         System.setProperty("webdriver.chrome.driver","/home/mrbasaran/Documents/chromedriver_linux64/chromedriver");
         WebDriver driver = new ChromeDriver();
-
         Page index = new Index(driver);
-
     }
 }
